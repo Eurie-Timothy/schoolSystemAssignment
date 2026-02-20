2b7915023529aa38928a512ceba09486a9329d68
fush <timothyeurie@gmail.com>
2026-02-20 17:35:33 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import classFiles.*;
 public class Main {
     public static void main(String[] args) {
-        Student student= new Student("Mark","474776","ibfevbyybwdfiubuy","9388uh8h488");
 
-        student.getDetails();
     }
 }
