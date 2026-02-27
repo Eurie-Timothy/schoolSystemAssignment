9237cc8bb97a05e370bb8df0b356b964e1f865de
fush <timothyeurie@gmail.com>
2026-02-27 12:34:01 +0300

    v2.1

--- src/classFiles/Lecturer.java
+++ src/classFiles/Lecturer.java
@@ -1,18 +1,19 @@
 package classFiles;
 public class Lecturer implements Person {
-    private String name;
+    private String firstName, lastName;
     private final String id;
     private String email;
     private String phoneNumber;
-    public Lecturer(String name, String id, String email, String phoneNumber) {
-        this.name = name;
+    public Lecturer(String firstName, String lastName, String id, String email, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.id = id;
         this.email= email;
         this.phoneNumber = phoneNumber;
     }
     @Override
     public String name() {
-        return this.name;
+        return this.firstName + " " + this.lastName;
     }
     @Override
     public String id() {
